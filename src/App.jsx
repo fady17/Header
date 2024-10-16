2e1fb16f469fbe125959ac292ff57995721b92e8
fady17 <fady17@github.com>
2024-10-17 00:43:30 +0300

    header configured with no assets using tailwind, lucide and react scroll ,clsx mob,Desktop responsive

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Header from "./sections/Header.jsx";
+
+const App = () => {
+    return (
+        <main className="overflow-hidden">
+            <Header />
+        </main>
+    )
+}
+export default App
